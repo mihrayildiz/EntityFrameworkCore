f6c19e508c118f71920b131e36b99db89080b7ce
mihrayldz <51748819+mihrayildiz@users.noreply.github.com>
2023-07-01 19:09:41 +0300

    Add files via upload

---
+++ Indexes/Program.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+ApplicationDbContext context = new();
+
+#region Index Nedir?
+//Index, bir sütuna dayalı sorgulamaları daha verimli ve performanslı hale getirmek için kullanılan yapıdır.
+#endregion
+#region Index'leme Nasıl Yapılır?
+//PK, FK ve AK olan kolonlar otomatik olarak indexlenir. 
+
+#region Index Attribute'u
+
+#endregion
+#region HasIndex Metodu
+
+#endregion
+#endregion
+#region Composite Index
+//context.Employees.Where(e => e.Name == "" || e.Surname == "")
+#endregion
+#region Birden Fazla Index Tanımlama
+
+#endregion
+#region Index Uniqueness
+
+#endregion
+#region Index Sort Order - Sıralama Düzeni (EF Core 7.0)
+
+#region AllDescending - Attribute
+//Tüm indexlemelerde descending davranışının bütünsel olarak konfigürasyonunu sağlar.
+#endregion
+#region IsDescending - Attribute
+//Indexleme sürecindeki her bir kolona göre sıralama davranışını hususi ayarlamak istiyorsak kullanılır.
+#endregion
+#region IsDescending Metodu
+
+#endregion
+#endregion
+#region Index Name
+
+#endregion
+#region Index Filter
+
+#region HasFilter Metodu
+
+#endregion
+#endregion
+#region Included Columns
+
+#region IncludeProperties Metodu
+
+#endregion
+#endregion
+
+
+
+//[Index(nameof(Name))] //name kolonuna index vermiş olduk.böylece name üzerinden yapılan işlemler daha az maliyetli ve yüksek erformans lı olur.
+//[Index(nameof(Surname))]
+//[Index(nameof(Name), nameof(Surname))] //comopsite indexleme
+//[Index(nameof(Name), AllDescending = true)]  //name indexi için değerler descending olarak başlayacak.
+//[Index(nameof(Name), nameof(Surname), IsDescending = new[] { true, false })] //nam için descending ancak surname için değil demek.
+//[Index(nameof(Name), Name = "name_index")]
+class Employee
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public int Salary { get; set; }
+}
+
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //modelBuilder.Entity<Employee>()
+        //.HasIndex(x => x.Name);
+        //.HasIndex(x => new { x.Name, x.Surname });
+        //.HasIndex(nameof(Employee.Name), nameof(Employee.Surname));
+        //.HasIndex(x => x.Name)
+        //.IsUnique();
+
+        //modelBuilder.Entity<Employee>()
+        //    .HasIndex(x => x.Name)
+        //    .IsDescending();
+
+        //modelBuilder.Entity<Employee>()
+        //    .HasIndex(x => new { x.Name, x.Surname })
+        //    .IsDescending(true, false);
+
+        //modelBuilder.Entity<Employee>()
+        //    .HasIndex(x => x.Name)
+        //    .HasDatabaseName("name_index");
+
+        //modelBuilder.Entity<Employee>()
+        //    .HasIndex(x => x.Name)
+        //    .HasFilter("[NAME] IS NOT NULL");  //şarta uymayan değerler idexedahil edilmez.
+
+        modelBuilder.Entity<Employee>()
+            .HasIndex(x => new { x.Name, x.Surname })
+            .IncludeProperties(x => x.Salary);  //idex dışındaki değerlere de ulaşmış oldum. ,ndexleme dışında da kolon getiriyorum sen vermliliği düşürme demiş oluyorum
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDB;User ID=SA;Password=1q2w3e4r+!;TrustServerCertificate=True");
+    }
+}
---
+++ Inheritance_Table_Per_Hierarchy/Program.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore;
+ApplicationDbContext context = new();
+#region Table Per Hierarchy (TPH) Nedir?
+//Kalıtımsal ilişkiye sahip olan entitylerin olduğu senaryolarda her bir hiyerarşiye karşılık bir tablo oluşturan davranıştır.
+#endregion
+#region Neden Table Per Hierarchy Yaklaşımında Bir Tabloya İhtiyacımız Olsun?
+//İÇerisinde benzer alanlara sahip olan entityleri migrate ettiğimizde her entitye karşılık bir tablo oluşturmaktansa bu entityleri tek bir tabloda modellemek isteyebilir ve bu tablodaki kayıtları discriminator kolonu üzerinden birbirlerinden ayırabiliriz. İşte bu tarz bir tablonun oluşturulması ve bu tarz bir tabloya göre sorgulama, veri ekleme, silme vs. gibi operasyonların şekillendirilmesi için TPH davranışını kullanabiliriz.
+#endregion
+#region TPH Nasıl Uygulanır?
+//EF Core'da entity aransında temel bir kalıtımsal ilişki söz konusuysa eğer default oalrak kabul edilen davranıştır.
+//O yüzden herhangi bir konfigüreasyon gerektirmez!
+//Entityler kendi aralarında kalıtımsal ilişkiye sahip olmalı ve bu entitylerin hepsi DbContext nesnesine DbSet olarak eklenmelidir! 
+#endregion
+#region Discriminator Kolonu Nedir?
+//Table Per Hierarchy yaklaşımı neticesinde kümülatif olarak inşa edilmiş tablonun hangi entitye karşılık veri tuttuğunu ayırt edebilmemizi sağlayan bir kolondur.
+//EF Core tarafından otomatik olarak tabloya yerleştirilir.
+//Default olarak içerisinde entity isimlerini tutar.
+//Discriminator kolonunu komple özelleştirebiliriz.
+#endregion
+#region Discriminator Kolon Adı Nasıl Değiştirilir?
+//Öncelikle hiyerarşinin başında hangi sınıf varsa onun Fluent API'da konfigürasyonuna gidilmeli.
+//Ardından HasDiscriminator fonksiyonu ile özelleştirilmeli.
+#endregion
+#region Discriminator Değerleri Nasıl Değiştirilir?
+//Yine hiyearşinin bşaındaki entity konfigürasyonlarına gelip, HasDiscriminator fonksiyonu ile özelleştirmede bulunarak ardından HasValue fonksiyonu ile hangi entitye karşılık hangi değerin girieceğini belirtilen türde ifade edebilirsiniz.
+#endregion
+#region TPH'da Veri Ekleme
+//Davranışların hiçbirinde veri eklerken,silerken, güncellerken vs. normal operasyonların dışında bir işlem yapılmaz!
+//Hangi davranışıo kullanıyorsanız EF Core ona göre arkaplanda modellemeyi gerçekleştirecektir.
+//Employee e1 = new() { Name = "Gençay", Surname = "Yıldız", Department = "Yazılım Bilgi İşlem" };
+//Employee e2 = new() { Name = "Nevin", Surname = "Yıldız", Department = "Yazılım Bilgi İşlem" };
+//Customer c1 = new() { Name = "Ahmet", Surname = "Bilmemne", CompanyName = "Ahmet Bilmemne Halı Kilim Yıkama" };
+//Customer c2 = new() { Name = "Şuayip", Surname = "XYZ", CompanyName = "Şuayip Sucuk" };
+//Technician t1 = new() { Name = "Rıfkı", Surname = "Kıllıbacak", Department = "Muhasebe", Branch = "Şöför" };
+//await context.Employees.AddAsync(e1); //Employees e e1 i ekledim.
+//await context.Employees.AddAsync(e2);
+//await context.Customers.AddAsync(c1);
+//await context.Customers.AddAsync(c2);
+//await context.Technicians.AddAsync(t1);
+
+//await context.SaveChangesAsync();
+#endregion
+#region TPH'da Veri Silme
+//TPH davranışında silme operasyonu yine entity üzerinden gerçekleştirilir.
+//var employee = await context.Employees.FindAsync(1);  //silmek istediğim nesneyi elde ettim.
+//Customers.FindAsync(1) desem hata alırım çünkü cusomers olup 1 idli veri yok.
+//context.Employees.Remove(employee);
+//await context.SaveChangesAsync();
+
+//var customers = await context.Customers.ToListAsync();
+//context.Customers.RemoveRange(customers);
+//await context.SaveChangesAsync();
+#endregion
+#region TPH'da Veri Güncelleme
+//TPH davranışında güncelleme operasyonu yine entity üzerinden gerçekleştirilir.
+//Employee guncellenecek = await context.Employees.FindAsync(8);
+//guncellenecek.Name = "Hilmi";
+//await context.SaveChangesAsync();
+#endregion
+#region TPH'da Veri Sorgulama
+//Veri sorgulama oeprasyonu bilinen DbSet propertysi üzerinden sorgulamadır. Ancak burada dikkat edilmesi gereken bir husus vardır. O da şu;
+//var employees = await context.Employees.ToListAsync();
+//var techs = await context.Technicians.ToListAsync();
+//kalıtımsal ilişkiye göre yapılan sorgulamada üst sınıf alt sınıftaki verileride kapsamaktadır. O yüzden üst sınıfların sorgulamalarında alt sınıfların verileride gelecektir buna dikkat edilmelidir.
+//Sorgulama süreçlerinde EF Core generate edilen sorguya bir where şartı eklemektedir.
+#endregion
+#region Farklı Entity'ler de Aynı İsimde Sütunların Olduğu Durumlar
+//Entitylerde mükerrer kolonlar olabilir. Bu kolonları EF core isimsel olarak özelleştirip ayıracaktır.
+#endregion
+abstract class Person  //abstract oladabilirdi olmayadabilirdi aynı durum geçerli.
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+}
+class Employee : Person
+{
+    public string? Department { get; set; }
+}
+class Customer : Person
+{
+    public int A { get; set; }
+    public string? CompanyName { get; set; }
+}
+
+class Technician : Employee  //Technician hem bir Employee hemde bir Person Employee de persondan
+                             //türediği için Technician da türedi.
+{
+    public int A { get; set; }
+    public string? Branch { get; set; }
+}
+
+class ApplicationDbContext : DbContext
+{
+
+    //enttityler dbset olarak verildi.entityler tanımlandı bu şekilde migration komutu yazılırsa THP ye uygun yapı oluşur.
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Technician> Technicians { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //modelBuilder.Entity<Person>()
+        //    .HasDiscriminator<string>("ayirici")  //Discriminator adını ayirici yaptık ve türünü string olarak vedik.
+        //    .HasValue<Person>("A")  //ayirici değeri eğer bir person ayıt atılıyorsa A olsun
+        //    .HasValue<Employee>("B") //Employee için değer atılıyorsa B olsun.
+        //    .HasValue<Customer>("C") //Customer için C olsun.
+        //    .HasValue<Technician>("D"); //Technician için D yazsın.
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDB;User ID=SA;Password=1q2w3e4r+!;TrustServerCertificate=True");
+    }
+}
