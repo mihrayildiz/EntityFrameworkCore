1b5759383f40924fce222e9ffc35583f91fb4f8e
mihrayldz <51748819+mihrayildiz@users.noreply.github.com>
2023-07-01 19:11:13 +0300

    Add files via upload

---
+++ Keyless_Entity_Types/Migrations/20221130171307_mig_2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KeylessEntityTypes.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder) //primary keyi olmayan bir sorgu çalıştırdık.
+        {
+            migrationBuilder.Sql($@" 
+                    CREATE VIEW vw_PersonOrderCount
+                    AS
+	                    SELECT p.Name, COUNT(*) Count FROM Persons p
+	                    JOIN Orders o 
+		                    ON p.PersonId = o.PersonId
+	                    GROUP By p.Name
+                ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"DROP VIEW vw_PersonOrderCount");
+        }
+    }
+}
